a1f3cf93c98ba91a2d4a8585f56f8fb383d6e03c
jaeyoung <hideyf@gmail.com>
2024-09-13 05:55:16 +0900

    필요없는 항목 삭제

--- AppStoreService/AppDTO.swift
+++ AppStoreService/AppDTO.swift
@@ -12,22 +12,16 @@ public struct AppDTO: Decodable, Equatable {
     public let artistId: Int
     public let artistName: String
     public let artworkUrl100: String
-    public let artworkUrl512: String
     public let averageUserRating: Float
     public let bundleId: String
-    public let contentAdvisoryRating: String
     public let description: String
     public let formattedPrice: String
-    public let languageCodesISO2A: [String]
     public let minimumOsVersion: String
     public let price: Float
-    public let primaryGenreName: String
-    public let releaseNotes: String
     public let screenshotUrls: [String]
     public let sellerName: String
     public let trackId: Int
     public let trackName: String
-    public let trackViewUrl: String
     public let version: String
     public let userRatingCount: Int
 }
