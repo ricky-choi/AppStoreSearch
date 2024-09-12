a9c116c8743b6448449d1a616cc789271d42e8fb
jaeyoung <hideyf@gmail.com>
2024-09-12 15:20:46 +0900

    AppStoreService CLI 추가

---
+++ AppStoreServiceRun/AppStoreRun.swift
@@ -0,0 +1,22 @@
+//
+//  AppStoreRun.swift
+//  AppStoreServiceRun
+//
+//  Created by Jaeyoung Choi on 9/12/24.
+//
+
+import Foundation
+import ArgumentParser
+import AppStoreService
+
+@main
+struct AppStoreRun: AsyncParsableCommand {
+    
+    @Argument(help: "검색어")
+    var term: String
+    
+    func run() async throws {
+        let results = try await AppStore.search(term: term)
+        print(results)
+    }
+}
