f2f62bcc4bd8f3c22770532b64e23c086450d01a
jaeyoung <hideyf@gmail.com>
2024-09-12 22:56:27 +0900

    Tab UI 초기화

--- AppStoreSearch/SceneDelegate.swift
+++ AppStoreSearch/SceneDelegate.swift
@@ -13,40 +13,46 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        
+        let vc0 = ViewController()
+        vc0.title = "투데이"
+        vc0.tabBarItem = .init(title: "투데이", image: UIImage(systemName: "doc.text.image"), tag: 1)
+        
+        let vc1 = ViewController()
+        vc1.title = "게임"
+        vc1.tabBarItem = .init(title: "게임", image: UIImage(systemName: "flag.filled.and.flag.crossed"), tag: 1)
+        
+        let vc2 = ViewController()
+        vc2.title = "앱"
+        vc2.tabBarItem = .init(title: "앱", image: UIImage(systemName: "square.stack.3d.up.fill"), tag: 1)
+        
+        let vc3 = ViewController()
+        vc3.title = "Arcade"
+        vc3.tabBarItem = .init(title: "Arcade", image: UIImage(systemName: "gamecontroller.fill"), tag: 1)
+        
+        let searchViewController = ViewController()
+        searchViewController.title = "검색"
+        searchViewController.tabBarItem = .init(title: "검색", image: UIImage(systemName: "magnifyingglass"), tag: 1)
+        
+        let tabBarController = UITabBarController()
+        
+        tabBarController.viewControllers = [
+            vc0.withNavigatioanController,
+            vc1.withNavigatioanController,
+            vc2.withNavigatioanController,
+            vc3.withNavigatioanController,
+            searchViewController.withNavigatioanController
+        ]
+        
+        tabBarController.selectedIndex = 4
+        
+        window?.rootViewController = tabBarController
+        
+        window?.makeKeyAndVisible()
+        
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
-
 }
 
---
+++ AppStoreSearch/UIViewController+.swift
@@ -0,0 +1,15 @@
+//
+//  UIViewController+.swift
+//  AppStoreSearch
+//
+//  Created by Jaeyoung Choi on 9/12/24.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    var withNavigatioanController: UINavigationController {
+        .init(rootViewController: self)
+    }
+}
